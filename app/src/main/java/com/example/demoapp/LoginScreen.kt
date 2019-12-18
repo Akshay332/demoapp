b20308ec4955ce789053c2692c0608d5c0564203
Akshay332 <munnapathania475@gmail.com>
2019-12-18 21:58:56 +0530

    update: gradient animator-list

--- app/src/main/java/com/example/demoapp/LoginScreen.kt
+++ app/src/main/java/com/example/demoapp/LoginScreen.kt
@@ -1,12 +1,18 @@
 package com.example.demoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login_screen.*
 
 class LoginScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_screen)
+        txtv_forgtpswd.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+
+        }
     }
 }
--- app/src/main/java/com/example/demoapp/SignUpActivity.kt
+++ app/src/main/java/com/example/demoapp/SignUpActivity.kt
@@ -1,13 +1,17 @@
 package com.example.demoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
-
+        signupBtn.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
