c8354e2435f1c65892e860b832d0f8ae413d618e
Akshay332 <munnapathania475@gmail.com>
2019-12-20 12:53:14 +0530

    update : home activity done with drawer and navigation views

--- app/src/main/java/com/example/demoapp/HomeActivity.kt
+++ app/src/main/java/com/example/demoapp/HomeActivity.kt
@@ -2,20 +2,19 @@ package com.example.demoapp
 
 import android.content.Intent
 import android.graphics.drawable.AnimationDrawable
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_home.*
-import kotlinx.android.synthetic.main.drawer.*
 
-class HomeActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener {
+class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     lateinit var toolbar: Toolbar
     lateinit var drawerLayout: DrawerLayout
@@ -27,7 +26,7 @@ class HomeActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelected
         toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        drawerLayout =findViewById(R.id.drawer_layout)
+        drawerLayout = findViewById(R.id.drawer_layout)
 
         navView = findViewById(R.id.nav_view)
 
@@ -38,7 +37,7 @@ class HomeActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelected
         toggle.syncState()
         navView.setNavigationItemSelectedListener(this)
 
-        val animationDrawable = Layout.getBackground() as AnimationDrawable
+        val animationDrawable = Layout.background as AnimationDrawable
         animationDrawable.setEnterFadeDuration(2000)
         animationDrawable.setExitFadeDuration(4000)
         animationDrawable.start()
@@ -46,14 +45,14 @@ class HomeActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelected
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.main,menu)
+        menuInflater.inflate(R.menu.main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId){
-            R.id.menu_logout ->{
-                startActivity(Intent(this,LoginScreenActivity::class.java))
+        return when (item.itemId) {
+            R.id.menu_logout -> {
+                startActivity(Intent(this, LoginScreenActivity::class.java))
                 Toast.makeText(this, "logout", Toast.LENGTH_LONG).show()
                 true
 
@@ -63,8 +62,8 @@ class HomeActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelected
     }
 
     override fun onNavigationItemSelected(p0: MenuItem): Boolean {
-        when(p0.itemId){
-            R.id.nav_profile ->{
+        when (p0.itemId) {
+            R.id.nav_profile -> {
                 Toast.makeText(this, "Profile clicked", Toast.LENGTH_SHORT).show()
             }
             R.id.nav_messages -> {
@@ -73,9 +72,9 @@ class HomeActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelected
             R.id.nav_friends -> {
                 Toast.makeText(this, "Friends clicked", Toast.LENGTH_SHORT).show()
             }
-            R.id.nav_signout ->{
-                Toast.makeText(this,"sign out clicked",Toast.LENGTH_SHORT).show()
-                startActivity(Intent(this,LoginScreenActivity::class.java))
+            R.id.nav_signout -> {
+                Toast.makeText(this, "sign out clicked", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this, LoginScreenActivity::class.java))
             }
         }
         drawerLayout.closeDrawer(GravityCompat.START)
