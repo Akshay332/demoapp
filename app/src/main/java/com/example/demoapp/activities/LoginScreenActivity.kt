ab6fb7f80ca80dd428adc7c30927209c81777fe5
Akshay332 <munnapathania475@gmail.com>
2019-12-22 16:00:39 +0530

    update:Signup db.helper connectivity

--- app/src/main/java/com/example/demoapp/activities/LoginScreenActivity.kt
+++ app/src/main/java/com/example/demoapp/activities/LoginScreenActivity.kt
@@ -1,35 +1,123 @@
 package com.example.demoapp.activities
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.demoapp.R
-import kotlinx.android.synthetic.main.activity_login_screen.*
+import com.example.demoapp.helper.InputValidation
+import com.example.demoapp.sql.DatabaseHelper
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
 
 class LoginScreenActivity : AppCompatActivity(), View.OnClickListener {
 
     private val activity = this@LoginScreenActivity
 
+    private lateinit var ConstraintLayoutLogin: ConstraintLayout
+
+    private lateinit var txtinputlayoutemail: TextInputLayout
+    private lateinit var edittxt_username: EditText
+
+    private lateinit var textinputlayoutpaswrd: TextInputLayout
+    private lateinit var edittext_pswd: EditText
+
+    private lateinit var txtv_forgtpswd: TextView
+
+    private lateinit var login_btn: Button
+
+    private lateinit var inputValidation: InputValidation
+    private lateinit var databaseHelper: DatabaseHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_screen)
-        txtv_forgtpswd.setOnClickListener (this)
-        login_btn.setOnClickListener (this)
+        txtv_forgtpswd.setOnClickListener(this)
+        login_btn.setOnClickListener(this)
+
+        // initializing the objects
+        initObjects()
+
+    }
+
+    /**
+     * This method is to initialize objects to be used
+     */
+    private fun initObjects() {
+
+        databaseHelper = DatabaseHelper(activity)
+        inputValidation = InputValidation(activity)
 
     }
 
     override fun onClick(v: View?) {
-        when(v!!.id){
-            R.id.txtv_forgtpswd ->{
+        when (v!!.id) {
+            R.id.txtv_forgtpswd -> {
+                // Navigate to signupActivity
                 startActivity(Intent(this, SignUpActivity::class.java))
             }
-            R.id.login_btn ->{
-                startActivity(Intent(this,
-                    HomeActivity::class.java))
-            }
+            R.id.login_btn -> verifyFromSQLite()
+
 
         }
+    }
+
+    /**
+     * This method is to validate the input text fields and verify login credentials from SQLite
+     */
+    private fun verifyFromSQLite() {
+        if (!inputValidation!!.isInputEditTextFilled(
+                edittxt_username!!,
+                txtinputlayoutemail!!,
+                getString(R.string.error_message_email)
+            )
+        ) {
+            return
+        }
+        if (!inputValidation!!.isInputEditTextEmail(
+                edittxt_username!!,
+                txtinputlayoutemail!!,
+                getString(R.string.error_message_email)
+            )
+        ) {
+            return
+        }
+        if (!inputValidation!!.isInputEditTextFilled(
+                edittext_pswd!!,
+                textinputlayoutpaswrd!!,
+                getString(R.string.error_message_email)
+            )
+        ) {
+            return
+        }
+        if (databaseHelper!!.checkUser(
+                edittxt_username!!.text.toString().trim { it <= ' ' },
+                edittext_pswd!!.text.toString().trim { it <= ' ' })
+        ) {
+
+            val accountsIntent = Intent(activity, HomeActivity::class.java)
+            accountsIntent.putExtra("EMAIL", edittxt_username!!.text.toString().trim { it <= ' ' })
+            emptyInputEditText()
+            startActivity(accountsIntent)
+        } else {
+            // Snack Bar to show success message that record is wrong
+            Snackbar.make(
+                ConstraintLayoutLogin!!,
+                getString(R.string.error_valid_email_password),
+                Snackbar.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    /**
+     * This method is to empty all input edit text
+     */
+    private fun emptyInputEditText() {
+        edittxt_username!!.text = null
+        edittext_pswd!!.text = null
     }
 }
--- app/src/main/java/com/example/demoapp/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/demoapp/activities/SignUpActivity.kt
@@ -1,13 +1,22 @@
 package com.example.demoapp.activities
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.example.demoapp.R
+import com.example.demoapp.helper.InputValidation
+import com.example.demoapp.model.User
+import com.example.demoapp.sql.DatabaseHelper
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
-class SignUpActivity : AppCompatActivity(),View.OnClickListener {
+class SignUpActivity : AppCompatActivity(), View.OnClickListener {
+
+    private val activity = this@SignUpActivity
+
+    private lateinit var inputValidation: InputValidation
+    private lateinit var databaseHelper: DatabaseHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,19 +24,102 @@ class SignUpActivity : AppCompatActivity(),View.OnClickListener {
 
         iv_backbtn.setOnClickListener(this)
         signupBtn.setOnClickListener(this)
+        txtv_alreadyaccount.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
-        when(v!!.id){
-            R.id.iv_backbtn ->{
-                startActivity(Intent(this,
-                    LoginScreenActivity::class.java))
+        when (v!!.id) {
+            R.id.iv_backbtn -> {
+                startActivity(
+                    Intent(
+                        this,
+                        LoginScreenActivity::class.java
+                    )
+                )
             }
-            R.id.signupBtn ->{
-                startActivity(Intent(this,
-                    HomeActivity::class.java))
-
-            }
+            R.id.signupBtn -> postDataToSQLite()
+            R.id.txtv_alreadyaccount -> finish()
         }
+    }
+
+    /**
+     * This method is to validate the input text fields and post data to SQLite
+     */
+    private fun postDataToSQLite() {
+        if (!inputValidation!!.isInputEditTextFilled(
+                editText_firstname,
+                text_input_layout_firstname,
+                getString(R.string.error_message_name)
+            )
+        ) {
+            return
+        }
+        if (!inputValidation!!.isInputEditTextFilled(
+                editText_lastname,
+                text_input_layout_lastname,
+                getString(R.string.error_message_name)
+            )
+        ) {
+            return
+        }
+
+        if (!inputValidation!!.isInputEditTextFilled(
+                editText_Email,
+                text_input_layout_email,
+                getString(R.string.error_message_email)
+            )
+        ) {
+            return
+        }
+        if (!inputValidation!!.isInputEditTextEmail(
+                editText_Email,
+                text_input_layout_email,
+                getString(R.string.error_message_email)
+            )
+        ) {
+            return
+        }
+        if (!inputValidation!!.isInputEditTextFilled(
+                editText_Paswrd,
+                text_input_layout_password,
+                getString(R.string.error_message_password)
+            )
+        ) {
+            return
+        }
+        if (!inputValidation!!.isInputEditTextMatches(
+                editText_Paswrd, editText_confirmPaswrd,
+                text_input_layout_confirmpaswd, getString(R.string.error_password_match)
+            )
+        ) {
+            return
+        }
+        if (!databaseHelper!!.checkUser(editText_Email!!.text.toString().trim())) {
+            var user = User(
+                Firstname = editText_firstname!!.text.toString().trim(),
+                Lastname = editText_lastname!!.text.toString().trim(),
+                email = editText_Email!!.text.toString().trim(),
+                password = editText_Paswrd!!.text.toString().trim()
+            )
+
+            databaseHelper!!.addUser(user)
+            // Snack Bar to show success message that record saved successfully
+            Snackbar.make(ConstraintLayoutSignup!!, getString(R.string.success_message), Snackbar.LENGTH_LONG).show()
+            emptyInputEditText()
+        }else {
+            // Snack Bar to show error message that record already exists
+            Snackbar.make(ConstraintLayoutSignup!!, getString(R.string.error_email_exists), Snackbar.LENGTH_LONG).show()
+        }
+
+    }
+    /**
+     * This method is to empty all input edit text
+     */
+    private fun emptyInputEditText() {
+        editText_firstname!!.text = null
+        editText_lastname!!.text = null
+        editText_Email!!.text = null
+        editText_Paswrd!!.text = null
+        editText_confirmPaswrd!!.text = null
     }
 }
